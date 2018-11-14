944734e68a0567f1708a0518b28a5a23fabba8d5
Laxman-M-R <45039533+Laxman-M-R@users.noreply.github.com>
2018-11-14 22:46:35 +0530

    Add files via upload

---
+++ PE_Problem_36_Solution.py
@@ -0,0 +1,7 @@
+sums = 0
+for i in range(1,1000000):
+    if str(i) == str(i)[::-1] and str(i)[0] != 0 and str(bin(i))[0] != 0 and str((bin(i)))[2:] == str((bin(i)))[:1:-1]:
+        sums += i
+        
+print(sums)
+
---
+++ PE_Problem_48_Solution.py
@@ -0,0 +1,7 @@
+sums = 0
+for a in range(1,1001):
+    sums += a**a
+
+c = str(sums)
+
+print(c[-10:])
